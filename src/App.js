266c0b9a42cb8826f9abef859dacdb9c88eb2f9e
unknown <psabarikrishnan@gmail.com>
2024-04-04 22:20:32 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@
 import './App.css';
 import './components/form'
 import Form from './components/form';
-import Transactionransactions from './components/transactions';
+
 
 function App() {
   return (
     <main>
       <Form/>
-      <Transactionransactions/>
+      
     </main>
 
   );
--- src/components/form.js
+++ src/components/form.js
@@ -5,50 +5,50 @@ function Form() {
     const [name, setname] = useState('');
     const [datetime, setdatetime] = useState('');
     const [desc, setdesc] = useState('');
-    const [transactions,setTransactions]=useState('');
-    useEffect(()=>{
+    const [transactions, setTransactions] = useState('');
+    useEffect(() => {
         getTransactions().then(setTransactions)
-    },[]);
+    }, []);
 
-    async function getTransactions(){
+    async function getTransactions() {
         const url = process.env.REACT_APP_API_URL + '/transactions';
         const response = await fetch(url);
-        return  await response.json();
+        return await response.json();
     }
 
     function addNewTransaction(ev) {
         ev.preventDefault();
         const url = process.env.REACT_APP_API_URL + '/transaction';
-        
+
         const price = name.split(' ')[0];
-    
+
         fetch(url, {
             method: 'POST',
             headers: { 'Content-type': 'application/json' },
             body: JSON.stringify({
                 price,
-                name:name.substring(price.length+1),
+                name: name.substring(price.length + 1),
                 datetime,
                 desc,
             })
         })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.json();
-        })
-        .then(json => {
-            setname('');
-            setdatetime('');
-            setdesc('');
-            console.log("result", json);
-        })
-        .catch(error => {
-            console.error('Error:', error);
-        });
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then(json => {
+                setname('');
+                setdatetime('');
+                setdesc('');
+                console.log("result", json);
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
     }
-    
+
 
     return (
         <div>
@@ -78,8 +78,28 @@ function Form() {
                     />
                 </div>
                 <button type='submit'>Add New Transaction</button>
-                
+
             </form>
+
+            <div className='transactions'>
+                {transactions && transactions.length > 0 && transactions.map(transaction => (
+                    <div className='transaction'>
+                        <div className='left'>
+                            <div className='name'>{transaction.name}</div>
+                            <div className='description'>{transaction.desc}</div>
+                        </div>
+                        <div className='right'>
+                            <div className={`price ${parseInt(transaction.price) < 0 ? 'red' : 'green'}`}>
+                                {transaction.price}
+                            </div>
+
+
+                            <div className='date-time'>{transaction.datetime}</div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+
         </div>
     );
 }
